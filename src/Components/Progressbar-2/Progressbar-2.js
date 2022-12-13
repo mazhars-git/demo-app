a3b371001410c6f1df7652ae3d587c627ef56cc5
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-12-13 23:33:42 +0600

    new prog bar

--- src/Components/Progressbar-2/Progressbar-2.js
+++ src/Components/Progressbar-2/Progressbar-2.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
 const Progressbar = () => {
-
+        const num = document.querySelector(".number");
+        let counter = 0;
+        setInterval(() => {
+        if (counter == 100) {
+            clearInterval();
+        } else {
+            counter += 1;
+            num.textContent = counter + "%";
+        }
+        }, 35);
 
     return(
         <>
-            
+            <div class="container">
+            <h2 class="header">Loading 3D Experience</h2>
+            {/* <!--  @  body section start --> */}
+            <div class="bar">
+                {/* <!--  $  Graphic section start --> */}
+                <svg>
+                <circle cx="50%" cy="50%" r="50%"></circle>
+                </svg>
+                {/* <!--  $  Graphic section End--> */}
+
+                {/* <!--  %  content section start --> */}
+                <h1 class="number">0%</h1>
+                <h4 class="config">Loading Robot</h4>
+                {/* <!-- %   content section End --> */}
+            </div>
+            {/* <!--  @  body section End--> */}
+            </div>
         </>
     );
 };
