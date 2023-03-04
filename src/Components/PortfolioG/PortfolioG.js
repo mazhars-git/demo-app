13bc5cbcbb26e4f3fb16eacb5831a20d775bca44
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-03-04 23:59:11 +0600

    update

--- src/Components/PortfolioG/PortfolioG.js
+++ src/Components/PortfolioG/PortfolioG.js
@@ -10,8 +10,6 @@ const PortfolioG = () => {
             button.addEventListener('click', (e) =>{
                 e.preventDefault();
                 const filter = e.target.dataset.filter
-                console.log('clicked')
-
                 storeImages.forEach((item) =>{
                     if(filter === 'all'){
                         item.style.display = 'block'
@@ -23,46 +21,44 @@ const PortfolioG = () => {
                             item.style.display = 'none'
                         }
                     }
-                })
-            })
-        })
-                
-
+                });
+            });
+        });
     })();
        
     return (
         <div>
             <div className="button-group filters-button-group">
-                <button className="button is-checked" data-filter="all">All</button>
-                <button className="button" data-filter="beach">Beach</button>
-                <button className="button" data-filter="boat">boat</button>
-                <button className="button" data-filter="lighthouse">lighthouse</button>
-                <button className="button" data-filter="people">people</button>
-                <button className="button" data-filter="rock">rock</button>
-                <button className="button" data-filter="sea">sea</button>
+                <button className="button is-checked" data-filter="all">ALL</button>
+                <button className="button" data-filter="beach">BEACH</button>
+                <button className="button" data-filter="boat">BOAT</button>
+                <button className="button" data-filter="lighthouse">LIGHTHOUSE</button>
+                <button className="button" data-filter="people">PEOPLE</button>
+                <button className="button" data-filter="rock">ROCK</button>
+                <button className="button" data-filter="sea">SEA</button>
             </div>
 
             <section id="grid-container" className="transitions-enabled fluid masonry js-masonry grid">
                 <article className="store-item beach people sea">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach1.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach1.jpg" className="img-fluid" />
                 </article>
                 <article className="store-item beach">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird3.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird3.jpg" className="img-fluid" />
                 </article>
                 <article className="store-item sea">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/sea2.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/sea2.jpg" className="img-fluid" />
                 </article>
                 <article className="beach people rock sea">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach2.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach2.jpg" className="img-fluid" />
                 </article>
                 <article className="beach sea">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach3.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach3.jpg" className="img-fluid" />
                 </article>
-                <article className="store-item bird">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird1.jpg" className="img-responsive" />
+                <article className="store-item beach">
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird1.jpg" className="img-fluid" />
                 </article>
-                <article className="store-item bird">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird2.jpg" className="img-responsive" />
+                <article className="store-item beach">
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/bird2.jpg" className="img-fluid" />
                 </article>
                 <article className="store-item sea">
                     <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/sea4.jpg" className="img-responsive" />
@@ -89,7 +85,7 @@ const PortfolioG = () => {
                     <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/sea1.jpg" className="img-responsive" />
                 </article>
                 <article className="store-item rock sea">
-                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/rock2.jpg" className="img-responsive" />
+                    <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/rock2.jpg" className="img-fluid" />
                 </article>
                 <article className="store-item beach">
                     <img src="https://cdn2.hubspot.net/hub/322787/hubfs/MYCHEFCOM/hackathon/photos-gallery/beach4.jpg" className="img-responsive" />
